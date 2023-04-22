c45db8a17b898ecd845398b11ba3a283be33e5f2
Hoang Nhan <hnhan793@gmail.com>
2023-04-23 00:01:27 +0700

    done header + carousel + responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // HEADER 
+
+    // --HEADER DARK MODE 
 function darkMode() {
     document.querySelector('.form__input').style.background = '#232220';
     document.querySelector('.form__input').style.color = '#fff';
@@ -8,9 +10,19 @@ function darkMode() {
 
 function lightMode() {
     document.querySelector('.form__input').style.background = '#fff';
+    document.querySelector('.form__input').style.color = 'rgba(22, 28, 45, 0.7)';
     document.querySelector('.header__light-icon').style.display = 'none';
     document.querySelector('.header__dark-icon').style.display = 'block';
 }
 
 document.querySelector('.header__dark-icon').onclick = darkMode;
 document.querySelector('.header__light-icon').onclick = lightMode;
+    
+    // --HEADER OPEN NAV 
+document.querySelector('.header__bar').onclick = function() {
+    document.querySelector('.header__nav').style.display = 'flex';
+}
+
+document.querySelector('.nav__close').onclick = function() {
+    document.querySelector('.header__nav').style.display = 'none';
+}
